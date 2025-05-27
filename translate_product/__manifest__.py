f59db738c689d27d787c68ed6fd793c2e71a8f79
TimourP <petit.timour@gmail.com>
2025-05-27 21:12:39 +0200

    feat: init

---
+++ translate_product/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Auto Translate Fields Product",
+    'summary': "Seamlessly translate any translatable fields in Odoo across all available languages with a single click.",
+    'description': """Product extension for otoolkit auto translate fields""",
+    'author': "Tipit",
+    'website': "https://otoolkit.app/apps/auto-translate-fields",
+    'category': 'Tools',
+    'version': '17.0.0.0.1',
+    'depends': ['product', 'otoolkit_auto_translate_fields'],
+    'maintainer': 'Tipit',
+    'support': 'contact@otoolkit.app',
+    'license': 'OPL-1',
+    'installable': True,
+    'application': False
+}
