cc5857dfb27289af1a1f39db3ec22d5ddaaba49a
TimourP <petit.timour@gmail.com>
2025-08-22 12:00:57 +0200

    feat: remove page

--- translate_website/models/__init__.py
+++ translate_website/models/__init__.py
@@ -1,4 +1,3 @@
 from . import website_blog
 from . import product_public_category
 from . import ir_ui_view
-from . import website_page
--- translate_website/models/website_page.py
+++
@@ -1,6 +0,0 @@
-from odoo import models
-
-
-class Page(models.Model):
-    _name = 'website.page'
-    _inherit = ['website.page', 'otk.translation.mixin']
